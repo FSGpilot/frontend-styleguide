c5e68d1d213e4c2e80989318b719c43d0117d7d0
sfa <sfa@netcompany.com>
2017-12-15 15:42:48 +0100

    Readded annotations javascript, fixed div size.

--- js/start.js
+++ js/start.js
@@ -102,5 +102,30 @@ $('.component-preview-button').on('click', function () {
 });
 
 
+// Annotations 
+$(document).ready(function () {
+  $('.marker').bind('click mouseenter', function (e) {
+    $(this).children('.desc').show();
+    $(this).children('.desc').css('z-index', '-1');
+    $(this).css('z-index', '1');
+  });
+
+  $('.marker').bind('mouseleave', function (e) {
+    $(this).children('.desc').hide();
+    $(this).css('z-index', 'auto');
+  });
+
+  //hide the description if clicked on anywhere on page
+  $(document).on('mouseup', function (e) {
+    var container = $('.desc');
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+      && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.hide();
+    }
+  });  
+});
+
 
 
